288dd9c5f4052c91f6949cd73f85b5257af270db
Precious Damisa <preciousdamisa@gmail.com>
2021-05-21 06:27:02 +0100

    Update

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/whatspool-frontend'));
+// If an incoming request uses a protocol other than HTTPS,
+// redirect that request to the same url but with HTTPS.
+const forceSSL = () => {
+  return (req, res, next) => {
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      return res.redirect(`https://${req.get("Host")}${req.url}`);
+    }
+    next();
+  };
+};
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/whatspool-frontend/index.html'));
+// Instruct the app to use the forceSSL middleware.
+app.use(forceSSL());
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/whatspool-frontend"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/whatspool-frontend/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
